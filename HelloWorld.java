3a6cf2ea913e31ca94f47853e2366097cb754aa5
riyatheresa18 <riya.abraham66@gmail.com>
2024-09-09 16:51:46 +0530

    Add HelloWorld.java file

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+// HelloWorld.java
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print a greeting message to the console
+        System.out.println("Hello, World!");
+    }
+}
